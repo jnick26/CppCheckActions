551606ac9762e31ef99bbd7a9f153ac27b172e69
Mykhailo Diachenko <jnick26@gmail.com>
2022-10-29 15:23:59 +0300

    Add failing test

--- tests/AddTest.cpp
+++ tests/AddTest.cpp
@@ -7,4 +7,8 @@ using ::testing::Eq;
 TEST(Add, when_two_numbers_added_then_result_returned) {
   EXPECT_THAT(lib::add(0, 0), Eq(0));
 }
+
+TEST(Add, when_two_numbers_added_then_result_returned_failing) {
+  EXPECT_THAT(lib::add(0, 0), Eq(42));
+}
 } // namespace
